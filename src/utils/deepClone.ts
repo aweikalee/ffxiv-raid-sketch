dd6533a4ec6645620478f183ec01d101a8594dfa
aweikalee <aweikalee@163.com>
2020-05-08 19:07:09 +0800

    feat: 添加deepClone, 将merge改为简单的浅拷贝

---
+++ src/utils/deepClone.ts
@@ -0,0 +1,17 @@
+/**
+ * 深拷贝
+ *
+ * 暂时只支持 普通object/array
+ * @param target object/array
+ */
+export function deepClone<T>(target: T): T {
+    if (typeof target === 'object') {
+        let cloneTarget = (Array.isArray(target) ? [] : {}) as T
+        for (const key in target) {
+            cloneTarget[key] = deepClone(target[key])
+        }
+        return cloneTarget
+    } else {
+        return target
+    }
+}
--- src/utils/merge.ts
+++ src/utils/merge.ts
@@ -1,11 +1,6 @@
 /**
- * 合并设置, b将会合并到a
+ * 合并对象, b将会合并到a
  */
 export function merge<T extends object>(a: T, b: Partial<T>) {
-    for (let i in b) {
-        if (b[i] !== undefined) {
-            a[i] = b[i]
-        }
-    }
-    return a
+    return Object.assign(a, b)
 }
