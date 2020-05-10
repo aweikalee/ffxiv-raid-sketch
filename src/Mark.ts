3b65df6858028351d148bd84ea06af08cff2b259
aweikalee <aweikalee@163.com>
2020-05-10 19:59:11 +0800

    chore: Mark props

--- src/Mark.ts
+++ src/Mark.ts
@@ -1,10 +1,11 @@
-import Layer, { ILayerEvent } from './Layer'
+import Layer, { ILayerEvent, ILayerState } from './Layer'
 import { ISketchUtils } from './Sketch'
 import { MARK } from './img/mark/map'
 import { MAKR_ALIAS, IMarkAlias } from './alias/mark'
 import { setAlias } from './alias/utils'
 import Img from './Img'
-// import { cloneDeep } from './utils'
+import { proxy, deepClone, merge } from './utils/index'
+import * as valid from './utils/vaildate'
 
 export interface IMarkProps {
     /**
@@ -14,7 +15,7 @@ export interface IMarkProps {
      *
      * 可通过 [[Mark.setAlias]] 设置别名
      */
-    type: IMarkAlias | null
+    type: IMarkAlias
 
     /**
      * 显示尺寸
@@ -27,51 +28,60 @@ export interface IMarkEvent extends ILayerEvent {
     size: (size: IMarkProps['size']) => void
 }
 
+const validator = valid.createValidator<IMarkProps>({
+    type(value) {
+        if (!isMarkAlias(value)) {
+            throw new Error('Mark.props.type is invalid')
+        }
+
+        return value
+    },
+    size(value) {
+        if (!valid.isNumber(value)) {
+            throw new Error('Mark.props.size must be a number')
+        }
+
+        return value
+    },
+})
+
 /**
  * 绘制 `目标标记`
  */
 export default class Mark extends Layer<IMarkEvent> {
-    /**
-     * 字段详情：[[IMarkProps]]
-     */
-    markProps: IMarkProps = {
-        type: null,
-        size: 7
-    }
-
+    props: IMarkProps
     private img = new Img()
 
-    constructor(type?: IMarkProps['type']) {
-        super()
+    constructor(
+        state: Partial<ILayerState> = {},
+        props: Partial<IMarkProps> = {}
+    ) {
+        super(state)
 
-        this.img.on('loaded', this.onChange.bind(this))
+        this.img.on('loaded', () => this.emit('change', []))
 
-        this.type(type || this.markProps.type)
+        this.props = proxyProps(this, {
+            type: 'attack1',
+            size: 7,
+        })
+
+        merge(this.props, props)
     }
 
     /**
      * 设置目标标记类型（名称）
      */
     type(value: IMarkProps['type']) {
-        if (!(value in MAKR_ALIAS)) return this
-        const _value = MAKR_ALIAS[value]
-        if (this.markProps.type === _value) return this
-
-        this.markProps.type = _value
-        this.emit('type', [_value])
-        return this.onChange()
+        this.props.type = value
+        return this
     }
 
     /**
      * 设置尺寸
      */
     size(value: IMarkProps['size']) {
-        if (typeof value !== 'number') return this
-        if (this.markProps.size === value) return this
-
-        this.markProps.size = value
-        this.emit('size', [value])
-        return this.onChange()
+        this.props.size = value
+        return this
     }
 
     /**
@@ -90,17 +100,46 @@ export default class Mark extends Layer<IMarkEvent> {
     }
 
     protected _clone() {
-        const layer = new Mark()
-        // layer.markProps = cloneDeep(this.markProps)
-        return layer
+        return new Mark(deepClone(this.state), deepClone(this.props))
     }
 
     protected _render(ctx: CanvasRenderingContext2D, utils: ISketchUtils) {
         const { img } = this
-        const { type, size } = this.markProps
+        const { type, size } = this.props
 
-        img.src(MARK[type])
+        img.src(MARK[type] || null)
         img.size(size)
         img.render(ctx, utils)
     }
+}
+
+/**
+ * @ignore
+ */
+function proxyProps(that: Mark, initialValue: IMarkProps) {
+    return proxy<IMarkProps>(
+        initialValue,
+        (key, oldValue, newValue, target) => {
+            validator(key, newValue, oldValue).then(
+                (value) => {
+                    if (key === 'type') {
+                        target['type'] = MAKR_ALIAS[value]
+                    }
+                    that.emit(key, [value] as any)
+                    that.emit('change', [])
+                },
+                (err) => {
+                    target[key] = oldValue
+                    throw err
+                }
+            )
+        }
+    )
+}
+
+/**
+ * @ignore
+ */
+function isMarkAlias(value: unknown): value is IMarkAlias {
+    return value in MAKR_ALIAS
 }
