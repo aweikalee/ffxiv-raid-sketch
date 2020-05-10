edf2a34e7e0afb661d6957e250eba612550a2b4f
aweikalee <aweikalee@163.com>
2020-05-10 20:19:23 +0800

    chore: Player props

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,12 @@
-import Layer, { ILayerEvent } from './Layer'
+import Layer, { ILayerEvent, ILayerState } from './Layer'
 import { ISketchUtils } from './Sketch'
 import { JOB, JOB_TYPE, JOB_COLOR } from './img/job/map'
 import { JOB_ALIAS, IJobAlias } from './alias/job'
 import { setAlias } from './alias/utils'
 import Img from './Img'
 import Circle from './Circle'
-// import { cloneDeep } from './utils'
+import { proxy, deepClone, merge } from './utils/index'
+import * as valid from './utils/vaildate'
 
 export interface IPlayerProps {
     /**
@@ -17,7 +18,7 @@ export interface IPlayerProps {
      *
      * 可通过 [[Player.setAlias]] 设置别名
      */
-    job: IJobAlias | null
+    job: IJobAlias
 
     /**
      * 显示尺寸
@@ -30,51 +31,61 @@ export interface IPlayerEvent extends ILayerEvent {
     size: (size: IPlayerProps['size']) => void
 }
 
+const validator = valid.createValidator<IPlayerProps>({
+    job(value) {
+        if (!isJobAlias(value)) {
+            throw new Error('Player.props.job is invalid')
+        }
+
+        return value
+    },
+    size(value) {
+        if (!valid.isNumber(value)) {
+            throw new Error('Player.props.size must be a number')
+        }
+
+        return value
+    },
+})
+
 /**
  * 绘制 职业图标
  */
 export default class Player extends Layer<IPlayerEvent> {
-    /**
-     * 字段详情：[[IPlayerProps]]
-     */
-    playerProps: IPlayerProps = {
-        job: null,
-        size: 5
-    }
+    props: IPlayerProps
     private img = new Img()
     private circle = new Circle()
 
-    constructor(job?: IPlayerProps['job']) {
-        super()
+    constructor(
+        state: Partial<ILayerState> = {},
+        props: Partial<IPlayerProps> = {}
+    ) {
+        super(state)
 
-        this.img.on('loaded', this.onChange.bind(this))
+        this.img.on('loaded', () => this.emit('change', []))
 
-        this.job(job || this.playerProps.job)
+        this.props = proxyProps(this, {
+            job: 'archer',
+            size: 5,
+        })
+
+        merge(this.props, props)
     }
 
     /**
      * 设置职业
      */
     job(value: IPlayerProps['job']) {
-        if (!(value in JOB_ALIAS)) return this
-        const _value = JOB_ALIAS[value]
-        if (this.playerProps.job === _value) return this
-
-        this.playerProps.job = _value
-        this.emit('job', [_value])
-        return this.onChange()
+        this.props.job = value
+        return this
     }
 
     /**
      * 设置尺寸
      */
     size(value: IPlayerProps['size']) {
-        if (typeof value !== 'number') return this
-        if (this.playerProps.size === value) return this
-
-        this.playerProps.size = value
-        this.emit('size', [value])
-        return this.onChange()
+        this.props.size = value
+        return this
     }
 
     /**
@@ -92,16 +103,14 @@ export default class Player extends Layer<IPlayerEvent> {
     }
 
     protected _clone() {
-        const layer = new Player()
-        // layer.playerProps = cloneDeep(this.playerProps)
-        return layer
+        return new Player(deepClone(this.state), deepClone(this.props))
     }
 
     protected _render(ctx: CanvasRenderingContext2D, utils: ISketchUtils) {
-        if (!this.playerProps.job) return
+        if (!this.props.job) return
         const { img, circle } = this
         const { strokeWidth } = this.state
-        const { job, size } = this.playerProps
+        const { job, size } = this.props
         const { unmapping } = utils
 
         const jobType = JOB_TYPE[job]
@@ -113,8 +122,39 @@ export default class Player extends Layer<IPlayerEvent> {
         circle.size(size / 2)
         circle.render(ctx, utils)
 
-        img.src(JOB[job])
+        img.src(JOB[job] || null)
         img.size(size - unmapping(strokeWidth * 2))
         img.render(ctx, utils)
     }
+}
+
+/**
+ * @ignore
+ */
+function proxyProps(that: Player, initialValue: IPlayerProps) {
+    return proxy<IPlayerProps>(
+        initialValue,
+        (key, oldValue, newValue, target) => {
+            validator(key, newValue, oldValue).then(
+                (value) => {
+                    if (key === 'job') {
+                        target['job'] = JOB_ALIAS[value]
+                    }
+                    that.emit(key, [value] as any)
+                    that.emit('change', [])
+                },
+                (err) => {
+                    target[key] = oldValue
+                    throw err
+                }
+            )
+        }
+    )
+}
+
+/**
+ * @ignore
+ */
+function isJobAlias(value: unknown): value is IJobAlias {
+    return value in JOB_ALIAS
 }
